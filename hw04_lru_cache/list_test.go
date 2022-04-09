8b0716fb73782e1b9216af332b9d93eda146e27f
Artem Novikov <a.novikov@securitycode.ru>
2022-04-09 07:01:24 +0300

    add some list tests

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -48,4 +48,45 @@ func TestList(t *testing.T) {
 		}
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
 	})
+
+	t.Run("remove items", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(1)
+		l.PushBack(2)
+		l.PushBack(3)       // [1,2,3]
+		l.Remove(l.Front()) // [2,3]
+
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, 2, l.Front().Value)
+		require.Equal(t, 3, l.Back().Value)
+		require.Nil(t, l.Front().Prev)
+
+		l.Remove(l.Back()) // [2]
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, l.Front(), l.Back())
+		require.Equal(t, 2, l.Front().Value)
+		require.Nil(t, l.Back().Next)
+	})
+
+	t.Run("move to front items", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(1)
+		l.PushBack(2)
+		l.PushBack(3) // [1,2,3]
+
+		middleItem := l.Front().Next
+		l.MoveToFront(middleItem) // [2,1,3]
+
+		require.Equal(t, l.Front(), middleItem)
+		require.Equal(t, 2, l.Front().Value)
+		require.Equal(t, 1, l.Front().Next.Value)
+		require.Nil(t, middleItem.Prev)
+
+		l.MoveToFront(l.Front()) // [2,1,3] nothing happened
+
+		require.Equal(t, 2, l.Front().Value)
+		require.Equal(t, l.Front(), middleItem)
+	})
 }
