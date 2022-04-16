51531b7577d3b6b54ad0f04b07998f184dd2ad74
Artem Novikov <a.novikov@securitycode.ru>
2022-04-16 20:39:13 +0300

    add Run method

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,47 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+func Consumer(tasks chan Task, limitErrorExceeded chan struct{}, errorsQuantity *int64, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for task := range tasks {
+		if err := task(); err != nil {
+			atomic.AddInt64(errorsQuantity, -1)
+		}
+	}
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	var wg sync.WaitGroup
+	errorsQuantity := int64(m)
+	limitErrorExceeded := make(chan struct{})
+	taskQueue := make(chan Task)
+
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go Consumer(taskQueue, limitErrorExceeded, &errorsQuantity, &wg)
+	}
+
+	for i := 0; i < len(tasks); i++ {
+		taskQueue <- tasks[i]
+		if atomic.LoadInt64(&errorsQuantity) <= 0 {
+			break
+		}
+	}
+
+	close(taskQueue)
+	wg.Wait()
+
+	if errorsQuantity <= 0 {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
