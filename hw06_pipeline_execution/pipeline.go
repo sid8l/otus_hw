c23381e7f5a3c2fa262ac9d2ef04b4eab6af8f79
Artem Novikov <a.novikov@securitycode.ru>
2022-05-10 03:07:59 +0300

    implement executePipiline func

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,32 @@ type (
 
 type Stage func(in In) (out Out)
 
+func DoneChecker(done In, in In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- v
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	interim := in
+	outStage := make(Out)
+	for _, stage := range stages {
+		outStage = stage(interim)
+		interim = outStage
+	}
+	outPrime := DoneChecker(done, outStage)
+	return outPrime
 }
