4ebf69f025e454ddc7fbae50a781d3536541cb89
Artem Novikov <a.novikov@securitycode.ru>
2022-08-18 23:15:08 +0300

    Implement Unpack function

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,42 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(inputString string) (string, error) {
+	var resultString strings.Builder
+	ra := []rune(inputString)
+	isCharacterEscaped := false
+	for i := range ra {
+		if unicode.IsDigit(ra[i]) && (i == 0 || unicode.IsDigit(ra[i-1]) && ra[i-2] != '\\') {
+			return "", ErrInvalidString
+		} else if unicode.IsDigit(ra[i]) && !isCharacterEscaped {
+			continue
+		}
+		if ra[i] == '\\' {
+			if i == len(ra) || !unicode.IsDigit(ra[i+1]) && ra[i+1] != '\\' {
+				return "", ErrInvalidString
+			} else if !isCharacterEscaped {
+				isCharacterEscaped = true
+				continue
+			}
+		}
+
+		if i < len(ra)-1 && unicode.IsDigit(ra[i+1]) {
+			nextNumber, err := strconv.Atoi(string(ra[i+1]))
+			if err != nil {
+				return "", err
+			}
+			resultString.WriteString(strings.Repeat(string(ra[i]), nextNumber))
+		} else {
+			resultString.WriteRune(ra[i])
+		}
+		isCharacterEscaped = false
+	}
+	return resultString.String(), nil
 }
