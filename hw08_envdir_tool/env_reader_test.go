6439fed1b294d7a29630997c517be1bd8b26065e
Artem Novikov <milosida@gmail.com>
2022-12-08 16:38:26 +0300

    Added ReadFile func< tests

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("negative", func(t *testing.T) {
+		env, err := ReadDir("fake/path")
+		require.Error(t, err)
+		require.Nil(t, env)
+
+		env, err = ReadDir("testdata/env/BAR")
+		require.Error(t, err)
+		require.Nil(t, env)
+	})
+
+	t.Run("positive", func(t *testing.T) {
+		env, err := ReadDir("testdata/env")
+		expected := Environment{
+			"BAR":   {Value: "bar", NeedRemove: false},
+			"EMPTY": {Value: "", NeedRemove: false},
+			"FOO":   {Value: "   foo\nwith new line", NeedRemove: false},
+			"HELLO": {Value: "\"hello\"", NeedRemove: false},
+			"UNSET": {Value: "", NeedRemove: true},
+		}
+		require.NoError(t, err)
+		require.Equal(t, expected, env)
+	})
 }
